bb37b548946e1dd5313f025e87a6006d39e0a6d2
radearsr <radea.surya1@gmail.com>
2022-11-18 08:31:20 +0700

    restucture code, add new pkg cheerio

---
+++ services/cheerio/CheerioService.js
@@ -0,0 +1,51 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+exports.checkUpdateAndGetLinkDownload = async (linkAnime, lastEpisode, timeout) => {
+  try {
+    const response = await axios.get(linkAnime, { timeout });
+    if (response.status !== 200) {
+      throw new Error("Status : ", response.status);
+    }
+    const $ = cheerio.load(response.data);
+    const allEpisodes = [];
+    $("#venkonten > div.venser > div:nth-child(8) > ul > li > span > a").each((_idx, el) => {
+      const link = $(el).attr("href");
+      allEpisodes.push(link);
+    });
+    if (allEpisodes.length > lastEpisode) {
+      return {
+        status: 1,
+        link: allEpisodes[0],
+      };
+    }
+    return {
+      status: 0,
+    };
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+exports.getLinkDownloadVideo720p = async (pageLink, timeout) => {
+  try {
+    const response = await axios.get(pageLink, { timeout });
+    if (response.status !== 200) {
+      throw new Error("Status : ", response.status);
+    }
+    const $ = cheerio.load(response.data);
+    const allLinkDownloads = [];
+    $("#venkonten > div.venser > div.venutama > div.download > ul:nth-child(2) > li:nth-child(3) > a").each((_idx, el) => {
+      const text = $(el).text();
+      const link = $(el).attr("href");
+      allLinkDownloads.push({
+        text,
+        link,
+      });
+    });
+    return allLinkDownloads;
+  } catch (error) {
+    throw error;
+  }
+}
--- services/telegram/TeleServices.js
+++ services/telegram/TeleServices.js
@@ -1,1 +1,33 @@
+const axios = require("axios");
 
+exports.sendNotifSuccess = async (teleToken, teleMessageId, title, episode, datas) => {
+  try {
+    const parsingData = datas.map((data) => {
+      return `Tempat : ${data.text}\nLink : ${data.link}`;
+    }).join("\n");
+    const message = `✅ ANIME UPDATE ✅\n>>${title}<<\n>>Episode ${episode}<<\n${parsingData}`;
+    await axios.get(`https://api.telegram.org/bot${teleToken}/sendMessage`, {
+      params: {
+        chat_id: teleMessageId,
+        text: message,
+      }
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+exports.sendNotifFailed = async (teleToken, teleMessageId, title, errorMsg, lineError) => {
+  try {
+    const message = `⛔ ${title} ⛔\n${errorMsg}\n${lineError}`;
+    await axios.get(`https://api.telegram.org/bot${teleToken}/sendMessage`, {
+      params: {
+        chat_id: teleMessageId,
+        text: message,
+      }
+    });
+  } catch (error) {
+    throw error;
+  }
+};

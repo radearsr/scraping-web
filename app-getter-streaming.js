8468fc5d8918badac3db523fc789a7b4d6a490f7
radearsr <radea.surya1@gmail.com>
2022-11-21 04:57:02 +0700

    update add timeout in calling recursive

--- app-getter-streaming.js
+++ app-getter-streaming.js
@@ -14,8 +14,12 @@ const getterStreaming = async () => {
         await mysqlService.insertLinkStreamingPage(mappedStreamingLinks);
         await mysqlService.updateStatusListAnime("1", animeLinks[0].id);
         const _animeLinks = await mysqlService.getLinkAnimes("0");
+        const time = 20000 * streamingLinks.length;
         if (_animeLinks.length > 0) {
-          await getterStreaming();
+          console.log("Aku masuk");
+          setTimeout( async () => {
+            await getterStreaming();
+          }, time);
         } else {
           await teleService.sendNotifFailed(process.env.BOT_TOKEN, process.env.GROUP_ID, "Selesai", streamingLinks, "10");
         }

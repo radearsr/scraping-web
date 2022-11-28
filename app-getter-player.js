a50b3314748cab44d150d0f2b433b8d2deb0dd19
radearsr <radea.surya4@gmail.com>
2022-11-28 15:37:30 +0700

    update else if error 500

--- app-getter-player.js
+++ app-getter-player.js
@@ -3,6 +3,9 @@ const mysqlService = require("./services/mysql/MysqlService");
 const teleService = require("./services/telegram/TeleServices");
 const reqVideo = "http://8.219.109.148:3000/getter";
 let errorLog = 0;
+let getOne = 0;
+let getTwo = 0;
+let getSpecial = 0;
 
 const getOneLinkAndInsert = async (idSource, link) => {
   try {
@@ -15,6 +18,12 @@ const getOneLinkAndInsert = async (idSource, link) => {
       const mappedVideo = [[idSource, video.data.result, null]];
       const affected = await mysqlService.insertLinkVideoPlayer(mappedVideo);
       return affected;
+    } else if (video.status === 500) {
+      if (getOne === 5) {
+        await mysqlService.updateSourceStreaming("2", idSource);
+        getOne = 0;
+      }
+      getOne++;
     }
   } catch (error) {
     await teleService.sendNotifFailed(process.env.BOT_TOKEN, process.env.GROUP_ID, "Get One Link And Insert", error, "10");
@@ -40,6 +49,12 @@ const getTwoLinkAndInsert = async (idSource, link, linkHD) => {
     } else if (videoHD.status === 200) {
        mappedVideo = [[idSource, null, videoHD.data.result]];
        affected = await mysqlService.insertLinkVideoPlayer(mappedVideo);
+    } else if (video.status === 500) {
+      if (getTwo === 5) {
+        await mysqlService.updateSourceStreaming("2", idSource);
+        getTwo = 0;
+      }
+      getTwo++;
     }
     return affected;
   } catch (error) {
@@ -48,7 +63,7 @@ const getTwoLinkAndInsert = async (idSource, link, linkHD) => {
   }
 }
 
-const getSpecialAndInsert = async (link) => {
+const getSpecialAndInsert = async (idSource, link) => {
   try {
     const videoHD = await axios.post(reqVideo, {
       link_streaming: link
@@ -57,6 +72,12 @@ const getSpecialAndInsert = async (link) => {
       const mappedVideo = [[idSource, videoHD.data.result, null]];
       const affected = await mysqlService.insertLinkVideoPlayer(mappedVideo);
       return affected;
+    } else if (videoHD.status === 500) {
+      if (getSpecial === 5) {
+        await mysqlService.updateSourceStreaming("2", idSource);
+        getSpecial = 0;
+      }
+      getSpecial++;
     }
   } catch (error) {
     await teleService.sendNotifFailed(process.env.BOT_TOKEN, process.env.GROUP_ID, "Get Special And Insert", error, "10");
